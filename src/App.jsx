035dc4d05ffe03fd242f8ca398b052828edf8111
Jesusalz <jesus.uculture@gmail.com>
2024-10-29 22:24:22 -0300

    Actualizacion Nombre

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Grupo 1</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          contar is {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
