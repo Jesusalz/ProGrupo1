b18eb6d89b7092b24e62a38e93b6451331f0e617
Jesusalz <jesus.uculture@gmail.com>
2024-11-08 09:54:30 -0300

    Componente Footer que se muestra al final de cada página del sitio web

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import TopHeader from './pages/home/components/TopHeader';
 import NavBar from './pages/home/components/navBar'; 
 import { HomePage, LoginPage, Register, AboutUs, NotFound } from './pages';
-
+import Footer from './pages/home/components/Footer'; 
 function App() {
   return (
     <>
@@ -15,6 +15,7 @@ function App() {
         <Route path="/about" element={<AboutUs />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
+      <Footer />
     </>
   );
 }
