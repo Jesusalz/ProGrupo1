40917a53e2e1247448c3e5fdae7445aa596acfc4
Jesusalz <jesus.uculture@gmail.com>
2024-11-12 20:29:51 -0300

    Agregada la api de Pixabay para imagenes

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,  // API desde el archivo .env
+  baseURL: import.meta.env.VITE_API_URL,  // API de productos desde el archivo .env
   headers: {
     "Content-Type": "application/json",  
   },
@@ -25,6 +25,23 @@ export const getProductById = async (id) => {
     return response.data;  
   } catch (error) {
     console.error('Error fetching product by ID:', error);
+    throw error;  
+  }
+};
+
+// Función para obtener imágenes de Pixabay
+export const getPixabayImages = async (query, limit = 8) => {
+  try {
+    const response = await axios.get(`https://pixabay.com/api/`, {
+      params: {
+        key: import.meta.env.VITE_PIXABAY_API_KEY,  // API key de Pixabay desde el archivo .env
+        q: query,
+        per_page: limit,
+      },
+    });
+    return response.data.hits;  // Devuelve las imágenes encontradas
+  } catch (error) {
+    console.error('Error fetching images from Pixabay:', error);
     throw error;  
   }
 };
