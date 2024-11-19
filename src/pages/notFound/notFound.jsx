c3a109196d6749c838f719940df2f2dabd69851c
TomasBozzano <tomycas10@gmail.com>
2024-11-19 19:39:40 -0300

    Creación del notFound

--- src/pages/notFound/notFound.jsx
+++ src/pages/notFound/notFound.jsx
@@ -1,5 +1,13 @@
+import { Link } from "react-router-dom";
+
 export function NotFound() {
   return (
-    <div>notFound</div>
+    <div className="flex flex-col justify-center items-center w-full gap-9 p-44">
+      <h1 className="text-6xl font-bold text-gray-900">404 - Not Found</h1>
+        <p> Your visited page not found. You may go home page</p>
+        <Link to="/"
+          className=" w-[100px] h-10 bg-red-500 text-center flex items-center justify-center rounded hover:bg-red-400 hover:text-white">
+          Go Home</Link>
+    </div>
   )
 }
