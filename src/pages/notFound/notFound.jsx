d792317434bf05c6498874ba27fa1aee6493f359
TomasBozzano <tomycas10@gmail.com>
2024-11-19 19:41:36 -0300

    Mejora visual

--- src/pages/notFound/notFound.jsx
+++ src/pages/notFound/notFound.jsx
@@ -6,7 +6,7 @@ export function NotFound() {
       <h1 className="text-6xl font-bold text-gray-900">404 - Not Found</h1>
         <p> Your visited page not found. You may go home page</p>
         <Link to="/"
-          className=" w-[100px] h-10 bg-red-500 text-center flex items-center justify-center rounded hover:bg-red-400 hover:text-white">
+          className=" w-[100px] bg-red-500 flex justify-center text-white py-1 rounded-md hover:bg-red-600 transition duration-300">
           Go Home</Link>
     </div>
   )
