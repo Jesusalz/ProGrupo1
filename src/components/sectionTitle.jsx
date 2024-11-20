386fa1b7d87139bba3d9b9024460760f6393a5b7
TomasBozzano <tomycas10@gmail.com>
2024-11-20 20:33:51 -0300

    Mejoras visuales

--- src/components/sectionTitle.jsx
+++ src/components/sectionTitle.jsx
@@ -1,7 +1,7 @@
 export function SectionTitle({nameCategory, title}) {
   return (
     <>
-    <div className="flex-row">
+    <div className="flex-row pl-24">
         <p className="flex text-red-500 gap-2 font-semibold">
         <div className="border-x-8 border-red-500 rounded text-red-500 text-2xl"></div>
            {nameCategory}</p>
