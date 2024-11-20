ec5d3c2321f4a8eea2589c1ca0e38de23efc837a
TomasBozzano <tomycas10@gmail.com>
2024-11-20 20:34:03 -0300

    implementación de carrusel

--- src/pages/home/homePage.jsx
+++ src/pages/home/homePage.jsx
@@ -3,8 +3,29 @@ import { useEffect } from 'react';
 import { fetchProducts } from "../../store/productsSlice";
 import Product from './components/Product';
 import { SectionTitle } from '../../components';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
 export function HomePage() {
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
   const dispatch = useDispatch();
   const { products = [], status, error } = useSelector((state) => state.products); 
 
@@ -14,9 +35,11 @@ export function HomePage() {
     }
   }, [status, dispatch]);
 
+  const productsCarousel = products.map((product) => <Product key={product.id} product={product} />);
+
   return (
-    <div className="container mx-auto p-4 w-full">
-      <div className='flex max-w-2xl pr-12 justify-center items-center'>
+    <div className="container mx-auto w-full p-10">
+      <div className='flex items-center pl-2'>
         < SectionTitle nameCategory="Today's" title="Productos" />
       </div>
 
@@ -26,13 +49,12 @@ export function HomePage() {
       {status === 'succeeded' && products.length === 0 && (
         <p>No hay productos disponibles.</p>
       )}
-
       {status === 'succeeded' && products.length > 0 && (
-        <div className="grid grid-cols-3 items-center justify-center max-w-3xl m-auto gap-4 pl-4">
-          {products.map((product) => (
-              <Product key={product.id} product={product} />
-          ))}
-        </div>
+        <div className="mx-auto w-5/6">
+          <Carousel responsive={responsive}>
+            {productsCarousel}
+          </Carousel>
+       </div>   
       )}
     </div>
   );
